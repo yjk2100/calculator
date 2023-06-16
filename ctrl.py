f7fd2ca23ab6517e2cf2ffd43babcf9a6ed9fb6a
developer_A <dev_A@abc.com>
2023-06-16 13:56:50 +0900

    Add sub function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -19,5 +19,8 @@ class Control:
         self.view.btn2.clicked.connect(self.view.clearMessage)
         
     def sum(self, a, b):
-            return a+b
+        return a+b
         
+    def sub(self, a, b):
+        return a-b
+    
